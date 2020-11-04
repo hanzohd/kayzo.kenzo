b8be787ebd2c5848f76352d57db7c13c1b165f8e
hanzohd <73936655+hanzohd@users.noreply.github.com>
2020-11-04 22:15:28 +0300

    kenzo.py
    سلام عليكم اول اداة لي شكرا لصديقي ايرور لمساعدتي

---
+++ kenzo.py
@@ -0,0 +1,22 @@
+import os
+print(" _        _______           _______  _______     _________ _______  _______  _       ")
+print("| \    /\(  ___  )|\     /|/ ___   )(  ___  )    \__   __/(  ___  )(  ___  )( \       ")
+print("|  \  / /| (   ) |( \   / )\/   )  || (   ) |       ) (   | (   ) || (   ) || (       ")
+print("|  (_/ / | (___) | \ (_) /     /   )| |   | | _____ | |   | |   | || |   | || |       ")
+print("|   _ (  |  ___  |  \   /     /   / | |   | |(_____)| |   | |   | || |   | || |       ")
+print("|  ( \ \ | (   ) |   ) (     /   /  | |   | |       | |   | |   | || |   | || |       ")
+print("|  /  \ \| )   ( |   | |    /   (_/\| (___) |       | |   | (___) || (___) || (____/\ ")
+print("|_/    \/|/     \|   \_/   (_______/(_______)       )_(   (_______)(_______)(_______/ ")
+print("1 for download mitasploit")
+print("2 for download Xerxes")
+print("3 for download Apk-Binder")
+print("4 for download fb-hacker")
+X = input("type number")
+if X =="1":
+    os.system("git clone https://github.com/rapid7/metasploit-framework")
+if X =="2":
+    os.system("git clone git clone https://github.com/zanyarjamal/xerxes")
+if X =="3":
+    os.system("git clone https://github.com/kinghacker0/Apk-Binder")
+if X =="4"
+    os.system("git clone https://github.com/V4N654T/fb-hacker")
